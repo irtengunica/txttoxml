e91071d2c4dc57618b6b9f4eaa25ecfba6d6774d
irtengunica <irtengunica@gmail.com>
2019-08-21 18:01:44 +0300

    Add files via upload

--- txttoxml.py
+++ txttoxml.py
@@ -3,10 +3,8 @@ import glob
 import os
 import random
 import time
-import cv2
-kaynakklasor='train_detect/labels/'
-hedefklasor='train_detect/labels/'
-resimklasör='train_detect/images/'
+kaynakklasor='detecnet_data/train_detect/labels/'
+hedefklasor='detecnet_data/train_detect/labels/'
 dosyalistesi=list(glob.glob(kaynakklasor+'*.txt'))
 dosyasayisi=len(dosyalistesi)-1
 for file in list(glob.glob(kaynakklasor+"*.txt")):
@@ -19,46 +17,31 @@ for file in list(glob.glob(kaynakklasor+"*.txt")):
  dosyaAdi = dosyaAdi.replace(".txt", "")
  resimadi=dosyaAdi+".jpg"
  xmladi=dosyaAdi+".xml"
- #print(dosyaAdi)
- #print(resimadi)
- #print(xmladi)
+ print(dosyaAdi)
+ print(resimadi)
+ print(xmladi)
  dosyasayisi=dosyasayisi-1
- img = cv2.imread(resimklasör+resimadi)
- height, width, channels = img.shape
- #print(height, width, channels)
- annotation = ET.Element("annotation")
- ET.SubElement(annotation, "folder").text = "train"
- ET.SubElement(annotation, "filename").text = str(resimadi)
- ET.SubElement(annotation, "path").text = str(resimklasör+resimadi)
- source = ET.SubElement(annotation, "source")
- ET.SubElement(source, "database").text = "Unknown"
- size = ET.SubElement(annotation, "size")
- ET.SubElement(size, "width").text = str(width)
- ET.SubElement(size, "height").text = str(height)
- ET.SubElement(size, "depth").text = str(channels)
- ET.SubElement(annotation, "segmented").text = "0"
- fihrist = open(kaynakklasor+dosyaAdi+".txt")
- satir1=fihrist.readlines()
- #print(satir1)
- for line in satir1:
-  Type = line.split()
-  nesneadi = Type[0]
-  x1 = Type[4]
-  y1 = Type[5]
-  x2 = Type[6]
-  y2 = Type[7]
-  object = ET.SubElement(annotation, "object")
-  ET.SubElement(object, "name").text = str(nesneadi)
-  ET.SubElement(object, "pose").text = "Unspecified"
-  ET.SubElement(object, "truncated").text = "0"
-  ET.SubElement(object, "difficult").text = "0"
-  bndbox = ET.SubElement(object, "bndbox")
-  ET.SubElement(bndbox, "xmin").text = str(x1)
-  ET.SubElement(bndbox, "ymin").text = str(y1)
-  ET.SubElement(bndbox, "xmax").text = str(x2)
-  ET.SubElement(bndbox, "ymax").text = str(y2)
- tree = ET.ElementTree(annotation)
- #tree.write("train/"+dosyaAdi+".xml",encoding="UTF-8",xml_declaration=True)
- tree.write("train/"+dosyaAdi+".xml")
+fihrist = open(kaynakklasor+"deneme"+".txt")
+satir1=fihrist.readlines()
+print(satir1)
+for line in satir1:
+ Type = line.split()
+ nesneadi = Type[0]
+ x1 = Type[4]
+ y1 = Type[5]
+ x2 = Type[6]
+ y2 = Type[7]
+ print(nesneadi,x1,y1,x2,y2)
 
+#satir1 = satir1.split()
+#satir1=satir1.split()
 
+#print(satir1[0])
+ #hedefdosya=secilendosya.replace(kaynakklasor, hedefklasor)
+# os.rename(kaynakklasor+dosyaAdi+".jpg",hedefklasor+"image"+str(i)+".jpg")
+# os.rename(kaynakklasor+dosyaAdi+".txt",hedefklasor+"image"+str(i)+".txt")
+# os.rename(kaynakklasor+dosyaAdi+".jpg",hedefklasor+"images/a"+dosyaAdi+".jpg")
+# os.rename(kaynakklasor+dosyaAdi+".txt",hedefklasor+"labels/a"+dosyaAdi+".txt")
+# time.sleep(1)
+#os.remove(kaynakklasor+dosyaAdi+".jpg")
+#os.remove(kaynakklasor+dosyaAdi+".txt")
